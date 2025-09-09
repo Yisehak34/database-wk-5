e210517d6b9f2b75eaa950f26dd23c4aedcaa4fa
Yisehak34 <yisehakgebeyehu34@gmail.com>
2025-09-09 08:49:11 +0300

    my w5 assignment

---
+++ answers.sql
@@ -0,0 +1,11 @@
+-- Answer 1
+DROP INDEX IdxPhone ON customers;
+
+-- Answer 2
+CREATE USER 'bob'@'localhost' IDENTIFIED BY 'S$cu3r3!';
+
+-- Answer 3
+GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
+
+-- Answer 4
+ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
